77422f6169808cace94d2a80ac9136012b51b277
Donato Laynes <donato.laynes@outlook.es>
2020-05-28 12:55:30 -0500

    Remove unused variable

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import App from './App.vue'
 import Fragment from 'vue-fragment'
 Vue.use(Fragment.Plugin)
 
-//Patron event bus
-Vue.prototype.$event = new Vue()
-
 Vue.config.productionTip = false
 
 new Vue({
